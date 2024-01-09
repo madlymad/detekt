b4211279787e1ab6e41be0c12d83175c911d18ee
Brais Gabín <brais.gabin@scrm.lidl>
2024-01-09 03:01:23 +0100

    Enable to use defaultConfig with allRules

--- detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/Analyzer.kt
+++ detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/Analyzer.kt
@@ -20,6 +20,7 @@ import io.gitlab.arturbosch.detekt.api.internal.whichDetekt
 import io.gitlab.arturbosch.detekt.api.internal.whichJava
 import io.gitlab.arturbosch.detekt.api.internal.whichOS
 import io.gitlab.arturbosch.detekt.core.config.AllRulesConfig
+import io.gitlab.arturbosch.detekt.core.config.CompositeConfig
 import io.gitlab.arturbosch.detekt.core.config.DisabledAutoCorrectConfig
 import io.gitlab.arturbosch.detekt.core.config.validation.DeprecatedRule
 import io.gitlab.arturbosch.detekt.core.config.validation.loadDeprecations
@@ -42,7 +43,7 @@ internal class Analyzer(
     private val processors: List<FileProcessListener>
 ) {
 
-    private val config: Config = settings.spec.workaroundConfiguration(settings.config)
+    private val config: Config = settings.spec.workaroundConfiguration(settings.baseConfig)
 
     fun run(
         ktFiles: Collection<KtFile>,
@@ -202,17 +203,16 @@ internal fun ProcessingSpec.workaroundConfiguration(config: Config): Config {
     var declaredConfig: Config = config
 
     if (rulesSpec.activateAllRules) {
-        val defaultConfig = getDefaultConfiguration()
         val deprecatedRules = loadDeprecations().filterIsInstance<DeprecatedRule>().toSet()
-        declaredConfig = AllRulesConfig(
-            originalConfig = declaredConfig,
-            defaultConfig = defaultConfig,
-            deprecatedRules = deprecatedRules
-        )
+        declaredConfig = AllRulesConfig(declaredConfig, deprecatedRules)
     }
 
     if (!rulesSpec.autoCorrect) {
         declaredConfig = DisabledAutoCorrectConfig(declaredConfig)
+    }
+
+    if (configSpec.useDefaultConfig) {
+        declaredConfig = CompositeConfig(declaredConfig, getDefaultConfiguration())
     }
 
     return declaredConfig
